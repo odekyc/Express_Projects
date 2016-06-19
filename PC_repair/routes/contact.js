9212f01ac60bd5325a8da58aa631c40cfeda3eb6
Elva Chao <odekyc@Elvas-Air.lan>
2016-06-19 16:39:52 -0700

    nodemailer added

--- PC_repair/routes/contact.js
+++ PC_repair/routes/contact.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 console.log("in contact");
 /* GET home page. */
@@ -11,6 +12,30 @@ router.post('/send', function(req, res){
 	console.log("hihi");
 
 	res.send("Form Submitted");
+
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'elvac8866@gmail.com',
+			pass: 'iloveeddiepeng'
+		}
+		});
+	
+	var mailOptions = {
+    from: 'Fred Foo 👥 <elvac8866@gmail.com>', // sender address
+    to: 'odekyc@gmail.com', // list of receivers
+    subject: 'Website Submission', // Subject line
+    text: 'You have  a submission with the following details....Name: '+req.body.name +' Email: '+req.body.email+' Message: '+ req.body.message, // plaintext body
+    html: '<b>Hello world 🐴</b>' // html body
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+        res.redirect('/');
+    }
+    console.log('Message sent: ' + info.response);
+});
   
 
 });
